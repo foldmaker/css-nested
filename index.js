51e3c9c931a61524625a071128d6086d3ee26049
Onur Kerimov <onurkerimov@gmail.com>
2020-02-03 00:27:52 +0300

    bump

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-import Foldmaker, { tokenize, traverseObjects } from 'foldmaker'
+import Foldmaker, { tokenize } from 'foldmaker'
 
-export let cssNested = string => {
+export default string => {
   // Tokenize the input
   let tokens = tokenize(
-    string,
+    string.replace(/\r\n/, '\n'),
     [
       ['i', /(\/\/).*?(?=\n|$)/], // Comment
       ['i', /\/\*[\s\S]*?\*\//], // Multiline comment
-      ['i', / +/], // spaces
-      ['i', /\n/],
+      ['i', / +|\n/], // spaces or newlines
       ['{', /{/],
       ['}', /}/],
       ['s', /^[^{}\n]+?,/], // Line ending in comma (most probably selector)
-      ['p', /^[\s\S]+?(;?)(?= ?[{}\n])/], // Selector or Property
-      ['u', /[\s\S]/] // Unknown
+      ['p', /^[\s\S]+?;?(?= ?[{}\n])/] // Selector or Property
     ],
     ({ type, value }) => {
       // This function ignores comments, multiline comments and spaces, right from the start
@@ -22,63 +20,31 @@ export let cssNested = string => {
     }
   )
 
-  let result = Foldmaker.from(tokens).parse(
-    [
-      ['BLOCK', /([s\n]*p)({[ops;\n]*?})/], // Block
-      ['UNKNOWN', /([\s\S])/] // Unknown
-    ],
-    {
-      BLOCK(result) {
-        let body = result[2]
-        let bodyMap = result.m[2]
-
-        bodyMap.split('').forEach((el, i) => {
-          switch (el) {
-            case 'p':
-              let temp = body[i]
-              // Adding semicolons after properties if necessary
-              if (temp[temp.length - 1] !== ';') body[i] = body[i] + ';'
-            case '{':
-              // Some formatting on the fly
-              body[i] = '  ' + body[i]
-            case '}':
-              body[i] = body[i] + '\n'
-              break
-            case 's':
-              body[i] = '  ' + body[i]
-          }
-        })
-        
-        // Store selector names as an array, later to be joined with commas
-        let selector = result[1].map(el => el[el.length - 1] === ',' ? el.substring(0, el.length - 1) : el)
-        return { selector, body }
-      }
-    }
-  )
-
-  let output = ''
-
-  let traverseCallback = obj => {
-    output +=
-      obj.selector.join(',\n').trim() + obj.body.filter(el => typeof el === 'string').join('')
-
-    traverseObjects(obj.body, childObj => {
-      // Do selector parent-child prefix magic
-      let parentSelector = obj.selector
-      let selector = childObj.selector
-      let newSelector = []
-      selector.forEach(sel => {
-        parentSelector.forEach(par => {
-          let newSel = /&/.exec(sel) ? sel.replace(/&/g, par.trim()) : par + ' ' + sel
-          newSelector.push(newSel)
-        })
-      })
-      childObj.selector = newSelector
-      traverseCallback(childObj)
+  return Foldmaker(tokens)
+    .parse(/(s*p)({[1ps]*?})/, result => {
+      let selectors = result[1].join('').split(',')
+      let body = result[2]
+      return { selectors, body }
     })
-  }
-  traverseObjects(result.array, traverseCallback)
-
-  // Finalize by adding newline characters where necessary
-  return output
+    .traverse((obj, also) => {
+      if (typeof obj === 'object') {
+        obj.body
+          .filter(child => typeof child === 'object')
+          .forEach(child => {
+            let newSelectors = []
+            obj.selectors.forEach(sel => {
+              child.selectors.forEach(ch => {
+                newSelectors.push(/&/.exec(ch) ? ch.replace(/&/g, sel.trim()) : sel + ' ' + ch)
+              })
+            })
+            child.selectors = newSelectors
+          })
+        also(obj.children)
+      }
+      return obj
+    })
+    .traverse((obj, also) => {
+      if (typeof obj === 'object') return obj.selectors.join(',\n') + '\n' + obj.body.map(also).join('\n')
+      else return obj
+    }).array[0]
 }
